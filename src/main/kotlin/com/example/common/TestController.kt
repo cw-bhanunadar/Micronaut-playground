698fa19e9eb248c1adada72d959e6db9b97de862
bhanunadar <bhanu.nadar@gmail.com>
2023-09-28 11:19:38 +0530

    Rabbit MQ Setup

--- src/main/kotlin/com/example/common/TestController.kt
+++ src/main/kotlin/com/example/common/TestController.kt
@@ -1,19 +1,24 @@
 package com.example.common
 
+import com.example.event.Publisher
 import com.example.util.logger
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.QueryValue
+import jakarta.inject.Inject
 
 @Controller("/test")
 class TestController {
     private val log = logger()
+
+    @Inject
+    lateinit var publisher: Publisher
     @Get("/simple/{params}")
-    suspend fun pathVariable(params: String?): String? {
+    suspend fun pathVariable(params: Long): Long? {
+        publisher.emitEventNumber(params)
         log.error("This is a test error")
         return params
     }
